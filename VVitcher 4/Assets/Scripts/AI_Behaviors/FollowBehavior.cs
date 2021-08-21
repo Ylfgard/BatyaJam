f4ea8ebcf6d656e2e1ef5aee039bf430203ff3ba
Ziamrurth <93211239@mail.ru>
2021-08-21 17:55:03 +0300

    Логика для приспешника(пока не наносит урона)

---
+++ VVitcher 4/Assets/Scripts/AI_Behaviors/FollowBehavior.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowBehavior : StateMachineBehaviour {
+    [SerializeField] private float _attackDistance;
+
+    private Transform _player;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Vector3.Distance(animator.transform.position, _player.position) < _attackDistance)
+        {
+            Debug.Log(Vector3.Distance(animator.transform.position, _player.position));
+            animator.SetTrigger("attack");
+        }
+        animator.GetComponent<NavMeshAgent>().SetDestination(_player.position);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<NavMeshAgent>().isStopped = true;
+        animator.GetComponent<NavMeshAgent>().ResetPath();
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
