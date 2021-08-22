b9688e6fd6992bec6d409e5d9252171a40581cfa
Ziamrurth <93211239@mail.ru>
2021-08-22 15:21:10 +0300

    Атака босса из состояния покоя

--- VVitcher 4/Assets/Scripts/AI_Behaviors/Archdemon/BossIdleBehavior.cs
+++ VVitcher 4/Assets/Scripts/AI_Behaviors/Archdemon/BossIdleBehavior.cs
@@ -2,18 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossIdleBehavior : StateMachineBehaviour
-{
+public class BossIdleBehavior : StateMachineBehaviour {
+    public float reloadTime = 3f;
+    public float rotationSpeed = 5f;
+
+    private float timeRemaining = 0f;
+
+    private Transform _player;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        timeRemaining = reloadTime;
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        Quaternion lookRotation = Quaternion.LookRotation((_player.position - animator.transform.position).normalized);
+        animator.transform.rotation = Quaternion.Slerp(animator.transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
 
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0)
+        {
+            timeRemaining = reloadTime;
+            animator.SetTrigger("Stage1");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
