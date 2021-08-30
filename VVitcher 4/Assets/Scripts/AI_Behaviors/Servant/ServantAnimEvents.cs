5294918061a514c045c3650b59bd211fd8e7b328
KomPotOD <ps_softpost@mail.ru>
2021-08-30 12:35:04 +0300

    ServantAnimDeathDestroyDelayEdit

--- VVitcher 4/Assets/Scripts/AI_Behaviors/Servant/ServantAnimEvents.cs
+++ VVitcher 4/Assets/Scripts/AI_Behaviors/Servant/ServantAnimEvents.cs
@@ -36,7 +36,7 @@ public class ServantAnimEvents : MonoBehaviour
 
     IEnumerator ServantDestroy()
     {
-        yield return new WaitForSeconds(.2f);
+        yield return new WaitForSeconds(.25f);
         Destroy(gameObject);
     }
 }
