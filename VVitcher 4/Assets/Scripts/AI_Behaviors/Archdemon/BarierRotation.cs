8d7a3cbd58e6d25f6d64e6284a33598d32f4eaa8
KomPotOD <ps_softpost@mail.ru>
2021-08-31 16:30:36 +0300

    Звук получения удара, звук агра серванта

--- VVitcher 4/Assets/Scripts/AI_Behaviors/Archdemon/BarierRotation.cs
+++ VVitcher 4/Assets/Scripts/AI_Behaviors/Archdemon/BarierRotation.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class BarierRotation : MonoBehaviour
 {
-    [SerializeField] Transform archdemon;
-    Quaternion rot;
+    private Quaternion rot;
 
-    private void Start()
-    {
-        rot = transform.rotation;
-    }
-
-    private void LateUpdate()
-    {
-        //transform.rotation = Quaternion.Euler(archdemon.rotation.x, archdemon.rotation.y, archdemon.rotation.z);
-
-        transform.rotation = rot;
-    }
+    private void Start() => rot = transform.rotation;
+    private void LateUpdate() => transform.rotation = rot;
 }
